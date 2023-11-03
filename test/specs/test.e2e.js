3beba432c99c205f251388bfe33e15c50a9e75e2
Andriy <andriy_polukhtovych@epam.com>
2023-11-03 13:19:39 +0200

    fix

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,17 +1,12 @@
 const { expect, browser, $ } = require('@wdio/globals')
 
 describe('My Login application', () => {
+   
     beforeEach(async() =>{
-        
         await browser.maximizeWindow();
-        
-    });
-
+     });
 
     // it ("Verify that allows register a User", async()=>{
-
-        
-
     //     const reg= await $(`.ico-register`);
     //     await reg.click();
     //     const fN= await $(`#FirstName`);
@@ -19,14 +14,11 @@ describe('My Login application', () => {
     //     const email=await $(`#Email`);
     //     const pass= await $(`#Password`);
     //     const conPass=await $(`#ConfirmPassword`)
-
     //     await fN.setValue("a");
     //     await lN.setValue("b");
     //     await email.setValue("A@fo.com");
     //     await pass.setValue(1234567);
     //     await conPass.setValue(1234567);
-    //     //await (`#gender-male`).click();
-
     //     const regb= await $(`#register-button`);
     //     await regb.click();
     //     await browser.pause(2000);
@@ -35,9 +27,6 @@ describe('My Login application', () => {
 
     it(`Verify that allows login a User`, async()=>{
         await browser.url("https://demowebshop.tricentis.com/");
-
-        // const log = await $(`.ico-logout`);
-        // await log.click();
         await browser.pause(2000);
         const login = await $(`.ico-login`);
         await login.click();
@@ -48,9 +37,7 @@ describe('My Login application', () => {
         await pass.setValue(1234567);
         await (await $(`[value="Log in"]`)).click();
         await browser.pause(2000);
-
         const acc= await $(`.account`);
-
         expect(await acc.getText()).toBe("A@ho.com");
     })
 
@@ -64,15 +51,13 @@ describe('My Login application', () => {
         expect( await desc.getText()).toBe("Desktops");
         expect( await note.getText()).toBe("Notebooks");
         expect( await acs.getText()).toBe("Accessories");
-
     })
+
     it(`Verify that allows sorting items (different options)`,async()=>{
         const acs1= await $$(`a[title="Show products in category Accessories"]`)[0]
-
         await acs1.click();
         await browser.pause(2000);
         const sortby= await $('#products-orderby');
-        // await browser.click(');
         await sortby.selectByVisibleText('Price: Low to High');
         await browser.pause(2000);
         const prices = await browser.execute(() => {
@@ -90,8 +75,8 @@ describe('My Login application', () => {
              });
         const sortName=  names.slice().sort();
         expect(names).toStrictEqual(sortName);
-
     })
+
       it(`Verify that allows changing number of items on page`,async()=>{
         const dropSize= await $(`#products-pagesize`);
         await dropSize.selectByVisibleText("4");
@@ -100,9 +85,8 @@ describe('My Login application', () => {
         await dropSize.selectByVisibleText("8");
         count = await $$('[class="item-box"]')
         expect(count.length).toBe(7)
-       
+      })
 
-    })
     it(`Verify that allows adding an item to the Wishlist`,async()=>{
         const prod= await $(`[href="/copy-of-tcp-self-paced-training"]`);
         await prod.click();
@@ -116,18 +100,12 @@ describe('My Login application', () => {
         const qw= await $(`input[class="qty-input valid"] `);
         const rows =await $$('[class="cart-item-row"]');
         const columns = rows[0].$$('td'); 
-        
-
-       const numberOfWishes=await columns[3].getText();
-
-
+        const numberOfWishes=await columns[3].getText();
         expect(numberOfWishes).toBe("TCP Public MT/AT")
-        
-
     })
+
     it(`Verify that allows adding an item to the card`,async()=>{
         await browser.url("https://demowebshop.tricentis.com/copy-of-tcp-self-paced-training");
-        
         const addButton= await $(`#add-to-cart-button-62`);
         await addButton.click();
         await browser.pause(2000);
@@ -136,17 +114,11 @@ describe('My Login application', () => {
         await browser.pause(2000);
         const rows1 =await $$('[class="cart-item-row"]');
         const columns1 = rows1[0].$$('td'); 
-        
-
-       const nameofprod=await columns1[2].getText();
-
-
+        const nameofprod=await columns1[2].getText();
         expect(nameofprod).toBe("TCP Public MT/AT")
-        
-    })
+     })
 
     it(`Verify that allows removing an item from the card`,async()=>{
-
         const chB= await $(`[name="removefromcart"]`);
         await chB.click();
         const upB= await $(`[name="updatecart"]`);
@@ -154,13 +126,10 @@ describe('My Login application', () => {
         await browser.pause(2000);
         const text=await $(`[class="order-summary-content"]`);
         expect(await text.getText()).toBe("Your Shopping Cart is empty!")      
-
     })
 
     it(`Verify that allows checkout an item `,async()=>{
-
         await browser.url("https://demowebshop.tricentis.com/copy-of-tcp-self-paced-training");
-        
         const addButton= await $(`#add-to-cart-button-62`);
         await addButton.click();
         await browser.pause(2000);
@@ -180,7 +149,6 @@ describe('My Login application', () => {
         await (await $('[id="BillingNewAddress_Address1"]')).setValue("Adress");
         await (await $('[id="BillingNewAddress_ZipPostalCode"]')).setValue(45678);
         await (await $('[id="BillingNewAddress_PhoneNumber"]')).setValue(1067);
-        
         await(await $$('[class="button-1 new-address-next-step-button"]')[0]).click();
         await browser.pause(1000);
         await(await $$('[class="button-1 new-address-next-step-button"]')[1]).click();
@@ -194,16 +162,6 @@ describe('My Login application', () => {
         await(await $('[class="button-1 confirm-order-next-step-button"]')).click();
         await browser.pause(1000);
         expect(await(await $('[class="title"]')).getText()).toBe('Your order has been successfully processed!')
-
-
-
-
-
-
     })
-
-
-
-
 })
 
